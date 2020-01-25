52d1a377086dcec1d606059c1590cdcd6092565f
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2020-01-25 16:42:32 +0200

    initial commit

--- Emailer/models.py
+++ Emailer/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Email(models.Model):
-    datetime_created = models.DateTimeField('Дата и время создания', auto_now=True)
+    datetime_created = models.DateTimeField('Дата и время создания', auto_now=True, editable=True)
     subject = models.CharField('Тема', max_length=100)
     message = models.CharField('Сообщение', max_length=500)
     seconds = models.PositiveIntegerField('Когда отправлять')
