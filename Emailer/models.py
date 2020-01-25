5edc78b5e9311ec1f48edbce625c8a5a8bc1abcf
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2020-01-25 12:59:15 +0200

    initial commit

--- Emailer/models.py
+++ Emailer/models.py
@@ -5,7 +5,7 @@ class Email(models.Model):
     datetime_created = models.DateTimeField('Дата создания')
     subject = models.CharField('Тема', max_length=100)
     message = models.CharField('Сообщение', max_length=500)
-    seconds = models.DateTimeField('Когда отправлять')
+    seconds = models.DateTimeField('Когда отправлять', null=True)
     to_whom = models.EmailField('Кому отправлять')
 
     class Meta:
