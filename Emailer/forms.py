55fed23f38208c95cc8104f3c5a3d747eea2ee54
Michael Kalashnikov <kalashnikovsystem@gmail.com>
2020-01-25 12:19:22 +0200

    initial commit

--- Emailer/forms.py
+++ Emailer/forms.py
@@ -4,10 +4,10 @@ from django import forms
 class EmailerForm(forms.Form):
     subject = forms.CharField(max_length=100)
     message = forms.CharField(widget=forms.Textarea)
-    seconds = forms.DateTimeField()
+    seconds = forms.FloatField()
     to_whom = forms.EmailField()
 
     subject.widget.attrs.update({'class': 'form-control'})
     message.widget.attrs.update({'class': 'form-control'})
-    # seconds.widget.attrs.update({'class': 'form-control'})
+    seconds.widget.attrs.update({'class': 'form-control'})
     to_whom.widget.attrs.update({'class': 'form-control'})
